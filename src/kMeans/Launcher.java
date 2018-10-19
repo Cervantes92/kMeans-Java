6a0007eab2e62179ef46b7a9ffa49502a9151448
Cervantes92 <petersonalex50@gmail.com>
2018-10-19 11:00:40 -0500

    Split string into string array.

--- src/kMeans/Launcher.java
+++ src/kMeans/Launcher.java
@@ -1,12 +1,30 @@
 package kMeans;
 
+import java.util.ArrayList;
+
 import kMeans.parser.Parser;
 
 public class Launcher {
 	
 	public static void main(String[] args) {
 		String centers = Parser.loadFileAsString("src/cluster-samples/cent.csv");
+		
+		//ArrayList<String> means = new ArrayList<>();
+		//means = Parser.loadFileAsArrayList("src/cluster-samples/cent.csv");
+		
+		System.out.println("Raw string input:");
 		System.out.println(centers);
+		
+		//System.out.println("ArrayList output");
+		//System.out.println(means);
+		
+		
+		String[] tokens = Parser.stringSplitsCSV(centers);
+		
+		System.out.println("Tokens after splitting: ");
+		for(int i = 0; i < tokens.length; i++) {
+			System.out.println(i + ": " + tokens[i]);
+		}
 	}
 
 }
--- src/kMeans/parser/Parser.java
+++ src/kMeans/parser/Parser.java
@@ -3,27 +3,46 @@ package kMeans.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Parser {
-	String line = "/n";
-	String splitBy = ",";
-
+	
+	//IMPORT FILE METHODS
 	public static String loadFileAsString(String path) {
 		StringBuilder builder = new StringBuilder();
+		String line;
 		
 		try {
 			BufferedReader  br = new BufferedReader(new FileReader(path));
-			String line;
-			while((line = br.readLine()) != null)
-				builder.append(line + "\n");
-			br.close();
+			while((line = br.readLine()) != null) {
+				builder.append(line + "/n");
+			}			
 		}
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		return builder.toString();	
+		return builder.toString();
 	}
 	
+	//In case of regex failure
+	public static ArrayList<String> loadFileAsArrayList(String path) {
+		ArrayList<String> result = new ArrayList<>();
+		String line;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			while((line = br.readLine()) != null) {
+				result.add(line);
+			}
+			br.close();			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//PARSE FILE METHOD
 	public static double parseDouble(String number) {
 		try {
 			return Double.parseDouble(number);
@@ -32,5 +51,15 @@ public class Parser {
 			e.printStackTrace();
 			return 0;
 		}
+	}
+	
+	public static String[] stringSplitsCSV(String str) {
+		//SplitBy regex is determined in this statement
+		String[] tokens = str.split("[,//n]+");
+		return tokens;
+	}
+	public static int countLines(String str) {
+		String[] lines = str.split("/r/n|/r|/n");
+		return lines.length;
 	}
 }
