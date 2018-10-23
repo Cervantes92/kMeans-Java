76600580a5e1968d78d46f1e642fb2ef1668382d
Cervantes92 <petersonalex50@gmail.com>
2018-10-23 12:46:00 -0500

    Draft of kMeans complete
    I have completed the first draft of the kMeans algorithm. Overall, the approach I used seems a bit too 'procedural'. I may revisit this at a different time to use a more object oriented approach. I have already began implementing the infrastructure for it.

--- src/kMeans/kMethods/kMethods.java
+++ src/kMeans/kMethods/kMethods.java
@@ -15,13 +15,11 @@ public class kMethods {
 		double minx = metrics.getMinXValue(), miny = metrics.getMinYValue(), 
 				maxx = metrics.getMaxXValue(), maxy = metrics.getMaxYValue();
 				
-		//How far the point moves.
-		double delta;
+		//Array to store the change in the center.
+		double[] delta = new double[numberOfClusters];
 		
 		//relation array expressing which point is related to what cluster
 		int[] relations = new int[clusterField[0].length];
-		
-
 		
 		//first guess for centers
 		double[][] centers = new double[2][numberOfClusters];
@@ -40,42 +38,87 @@ public class kMethods {
 		double[][] distances = new double[clusterField[0].length][numberOfClusters];
 		
 		//Store smallest distance.
-		double[] smallestDistance;
+		double[] smallestDistance = new double[clusterField[0].length];
 		
 		//TODO: Check null in algorithm's main loop. IDE bug prevents checking for null values in a doubles array.
 		for(int i = 0; i < clusterField[0].length; i++) {
 			for(int j = 0; j < centers[0].length; j++) {
-				smallestDistance[i] = distances[i][j];
+				smallestDistance[i] = calculateDistance(clusterField[0][i],clusterField[1][i],centers[0][j],centers[1][j]);
 			}
 		}
 		
+		//Variables for calculating the average.
+		double sumx = 0;
+		double sumy = 0;
+		double num = 0; //Double to avoid any rounding errors.
+		
+		//Main algorithm loop
 		do {
+			
 			//Assign each point it's nearest center.
 			for(int i = 0; i < clusterField[0].length; i++) {
 				for(int j = 0; j < centers[0].length; j++) {
+					
 					//calculate the distance between each point and each center;
 					distances[i][j] = calculateDistance(clusterField[0][i],clusterField[1][i],centers[0][j],centers[1][j]);
 					
-					//Store the smallest distance for the 
-					
-					//Store the index of the closest center. TODO: null check should go here!
-					if(distances[i][j] < smallestDistance[i] || smallestDistance[i] == null) {
-						
+					//Store the index of the closest center.
+					if(distances[i][j] < smallestDistance[i]) {
+						smallestDistance[i] = distances[i][j];
+						relations[i] = j;
 					}
+				}
+				
+				//Set the new centers. IDE doesn't recognize that j has already been declared.
+				for(int j = 0; j < centers[0].length; j++) {
+					sumx = 0;
+					sumy = 0;
+					num = 0;
+					
+					for(i = 0; i < clusterField[0].length; i++) {
+						
+						//Check if the point is assigned to the cluster.
+						if(relations[i] == j) {
+							//add to running sum
+							sumx += clusterField[0][i];
+							sumy += clusterField[1][i];
+							num += 1;
+						}
+					}
+					
+					//Set the new center to the average.
+					centersOld[0][j] = centers[0][j];
+					centersOld[1][j] = centers[1][j];
+					
+					centers[0][j] = sumx / num;
+					centers[1][j] = sumy / num;
+					
+					//calculate delta
+					delta[j] = deltaCalc(centers[0][j], centers[1][j], centersOld[0][j], centersOld[1][j]);
 					
 				}
 			}
-			//Calculate change in center positions.
-			delta = 0;
-			
 		}
-		while(delta > stability);
+		
+		while(sumArray(delta) > stability);
 		
 		
 		return relations;
 	}
 	
-	public static double calculateDistance(double x1, double y1, double x2, double y2) {
+	private static double calculateDistance(double x1, double y1, double x2, double y2) {
 		return Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+	}
+	
+	private static double sumArray(double[] someArray) {
+		double sum = 0;
+		for(int i = 0; i < someArray.length; i++)
+			sum += someArray[i];
+		return sum;
+	}
+	
+	private static double deltaCalc(double x1, double y1, double x2, double y2) {
+		double delta = ((x2 - x1) + (y2 - y1)) / 2;
+		return Math.abs(delta);
 	}
 }
