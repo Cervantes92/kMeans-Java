9d2c665f6bb0a79c6aa1ebdd373cd265d1567370
Cervantes92 <petersonalex50@gmail.com>
2018-10-30 16:59:54 -0500

    Looping issue fixed
    Issue regarding the failure of the program to iterate through every point has been resolved.
    
    KRrr it was a misplaced curly bracket.

--- src/kMeans/Launcher.java
+++ src/kMeans/Launcher.java
@@ -9,11 +9,6 @@ public class Launcher {
 		//Load file as string
 		String centers = Parser.loadFileAsString("src/cluster-samples/cent.csv");
 		
-		//SANITY CHECK
-		System.out.println("Raw string input:");
-		System.out.println(centers);
-		System.out.println();
-		
 		//Break String into lines
 		String[] tokens = Parser.stringSplitsCSV(centers);
 		
@@ -36,7 +31,7 @@ public class Launcher {
 		
 		//Execute kMeans algorithm
 		double[][] centerPoints = new double[2][points[0].length];
-		centerPoints = kMethods.kMeans(points, 3, 0.01);
+		centerPoints = kMethods.kMeans(points, 3, 0);
 		
 		//Print out results
 		System.out.println("i\t\t\tx\t\t\ty");
--- src/kMeans/kMethods/kMethods.java
+++ src/kMeans/kMethods/kMethods.java
@@ -10,14 +10,13 @@ public class kMethods {
 	public static double[][] kMeans(double[][] clusterField, int numberOfClusters, double stability) {
 		fieldMetrics metrics = new fieldMetrics(clusterField);
 		
-		//Declare loop indexes to work around nested loop index declaration issue. How could this go wrong?
+		//Declare loop indexes to work around nested loop index declaration issue because I like being consistent with my indexes.
 		int i;
 		int j;
 		
 		//Set size of cluster field.
 		double minx = metrics.getMinXValue(), miny = metrics.getMinYValue(), 
 				maxx = metrics.getMaxXValue(), maxy = metrics.getMaxYValue();
-		//System.out.println("minX: " + minx + "\nmaxX: " + maxx + "\nminY: " + miny + "\nmaxY: " + maxy);
 				
 		//Array to store the change in the center.
 		double[] delta = new double[numberOfClusters];
@@ -29,7 +28,6 @@ public class kMethods {
 		double[][] centers = new double[2][numberOfClusters];
 		
 		//Draw a line of potential centers across the field
-		//TODO: Have the initial conditions scale with the cluster field space.
 		double m = ((maxx - minx) / (maxy - miny));
 		for(i = 0; i < centers[0].length; i++) {
 			centers[0][i] = m * i / centers[0].length + minx;
@@ -45,7 +43,7 @@ public class kMethods {
 		//Store smallest distance.
 		double[] smallestDistance = new double[clusterField[0].length];
 		
-		//TODO: Check null in algorithm's main loop. IDE bug prevents checking for null values in a doubles array.
+		//TODO: Check null in algorithm's main loop.
 		for(i = 0; i < clusterField[0].length; i++) {
 			for(j = 0; j < centers[0].length; j++) {
 				smallestDistance[i] = calculateDistance(clusterField[0][i],clusterField[1][i],centers[0][j],centers[1][j]);
@@ -55,21 +53,24 @@ public class kMethods {
 		//Variables for calculating the average.
 		double sumx = 0;
 		double sumy = 0;
-		double num = 0; //Double to avoid any rounding errors.
+		double num = 0; //Double to avoid rounding errors.
 		
 		//Main algorithm loop
 		do {
 			
-			/*Assign each point it's nearest center. NOTE: here is what is happening. All three potential centers are
-			 *being assigned the same point. I must implement a check to ensure that each point is only assigned one
-			 *center.
-			 */
-			
+			//Assign each point it's nearest center.
 			System.out.println("Distance calc: ");
 			for(i = 0; i < clusterField[0].length; i++) {
+				
+				//Diagnostics
+				System.out.println("For point: " + i);
+				
 				for(j = 0; j < centers[0].length; j++) {
 					
-					//calculate the distance between each point and each center;
+					//Diagnostics
+					System.out.println("in relation to center: " + j);
+					
+					//calculate the distance between each point and each center
 					distances[i][j] = calculateDistance(clusterField[0][i],clusterField[1][i],centers[0][j],centers[1][j]);
 					
 					//Diagnostics
@@ -79,47 +80,50 @@ public class kMethods {
 					if(distances[i][j] < smallestDistance[i]) {
 						smallestDistance[i] = distances[i][j];
 						relations[i] = j;
+						System.out.println("CHANGE!");
 					}
-				}
-				
-				//Diagnostics
-				System.out.println();
-				System.out.println("Relations[pointIndex] = centerIndex: ");
-				for(i = 0; i < relations.length; i++) {
-					System.out.println(i + " : " + relations[i]);
-				}
-				System.out.println();
-				
-				//Set the new centers. IDE doesn't recognize that j has already been declared.
-				for(j = 0; j < centers[0].length; j++) {
-					sumx = 0;
-					sumy = 0;
-					num = 0;
 					
-					for(i = 0; i < clusterField[0].length; i++) {
+					//Diagnostics
+					System.out.println("The smallest distance was: " + smallestDistance[i]);
+				}
+			}
+			//Diagnostics
+			System.out.println();
+			System.out.println("Relations[pointIndex] = centerIndex: ");
+			for(i = 0; i < relations.length; i++) {
+				System.out.println(i + " : " + relations[i]);
+			}
+			System.out.println();
+				
+			//Set the new centers. IDE doesn't recognize that j has already been declared.
+			for(j = 0; j < centers[0].length; j++) {
+				sumx = 0;
+				sumy = 0;
+				num = 0;
+					
+				for(i = 0; i < clusterField[0].length; i++) {
 						
-						//Check if the point is assigned to the cluster.
-						if(relations[i] == j) {
-							//add to running sum
-							sumx += clusterField[0][i];
-							sumy += clusterField[1][i];
-							num += 1;
-						}
+					//Check if the point is assigned to the cluster.
+					if(relations[i] == j) {
+						//add to running sum
+						sumx += clusterField[0][i];
+						sumy += clusterField[1][i];
+						num += 1;
 					}
+				}
 					
 					//Set the new center to the average.
-					for(int thing = 0; thing < centers[0].length; thing++) {
-						centersOld[0][thing] = centers[0][thing];
-						centersOld[1][thing] = centers[1][thing];
-					}
-					
-					centers[0][j] = sumx / num;
-					centers[1][j] = sumy / num;
-					
-					//calculate delta
-					delta[j] = deltaCalc(centers[0][j], centers[1][j], centersOld[0][j], centersOld[1][j]);
-					
+				for(int thing = 0; thing < centers[0].length; thing++) {
+					centersOld[0][thing] = centers[0][thing];
+					centersOld[1][thing] = centers[1][thing];
 				}
+					
+				centers[0][j] = sumx / num;
+				centers[1][j] = sumy / num;
+					
+				//calculate delta
+				delta[j] = deltaCalc(centers[0][j], centers[1][j], centersOld[0][j], centersOld[1][j]);
+					
 			}
 		}
 		
